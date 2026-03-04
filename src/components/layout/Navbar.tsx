dbbfe5dc15b6ec5379c3993c4837ac6cdcd48242
vjegorovs00 <v.jegorovs00@gmail.com>
2026-03-04 21:14:21 +0300

    Working on Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,27 @@
 'use client';
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import Image from "next/image";
+
+import chevronLeft from "@/assets/layout/chevronLeft.png"
+import logoNavbar from "@/assets/logo/logoNavbar.svg"
 
 export default function Navbar(){
+    const router = useRouter();
+
     return(
-        <header>
-            
+        <header className="px-8 pt-4 pb-3 border-b">
+            <div className="relative flex items-center justify-center">
+                <button
+                    type="button"
+                    onClick={() => router.back()}
+                    aria-label="Go back"
+                    className="absolute left-0 flex h-8 w-8 items-center justify center rounded-full" 
+                >
+                    <Image src={chevronLeft} alt="Left arrow - back button"/>
+                </button>
+                <Image src={logoNavbar} alt="Flori logo"/>
+            </div>
         </header>
     );
 }
