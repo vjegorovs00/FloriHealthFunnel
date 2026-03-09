48498cce951483cdbe92b67a2790fc60cc48a4c8
vjegorovs00 <v.jegorovs00@gmail.com>
2026-03-09 19:06:41 +0300

    Implemented template for step3 -> 4

--- src/app/(wizard)/step-3/page.tsx
+++ src/app/(wizard)/step-3/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { MetricNumberStep } from "@/components/wizard/MetricNumberStep";
+
 export default function Step3Page(){
+    const router = useRouter();
+
     return(
-        <div>hello! Step 3</div>
+        <div>
+            <MetricNumberStep
+                title="What is your weight?"
+                label="Height"
+                metricUnit="kg"
+                imperialUnit="lb"
+                onSubmit={async ({system, value}) => {
+                    console.log('height step', {system, value})
+                    router.push('/step-4')
+                }}
+            />
+        </div>
     );
 }
--- src/app/(wizard)/step-4/page.tsx
+++ src/app/(wizard)/step-4/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+import { useRouter } from "next/navigation";
+import { MetricNumberStep } from "@/components/wizard/MetricNumberStep";
+
 export default function Step4Page(){
+    const router = useRouter();
+
     return(
-        <div>hello! Step 4</div>
+        <div>
+            <MetricNumberStep
+                title="What is your dream weight?"
+                label="Height"
+                metricUnit="kg"
+                imperialUnit="lb"
+                onSubmit={async ({system, value}) => {
+                    console.log('height step', {system, value})
+                    router.push('/step-special')
+                }}
+            />
+        </div>
     );
 }
